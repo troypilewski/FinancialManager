5a269602368741cccf1a146acae1c837329c4853
Troy Pilewski <12568877+troypilewski@users.noreply.github.com>
2023-10-29 19:11:10 -0700

    Initial Commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,10 @@
+module com.fm.financemanager {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.fm.financemanager to javafx.fxml;
+    exports com.fm.financemanager;
+    exports com.fm.financemanager.controller;
+    opens com.fm.financemanager.controller to javafx.fxml;
+}
